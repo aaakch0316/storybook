b0ad8f11c364b5abc64013f28ea3a74a9c40fde7
hanpy <hanpy@deepbrainai.io>
2023-02-09 18:56:45 +0900

    feact : common color settings

--- src/styles/common/colors.js
+++ src/styles/common/colors.js
@@ -1,122 +1,120 @@
-export default rootColors = {
-  grey50: "#f9fafb",
-  grey100: "#f2f4f6",
-  grey200: "#e5e8eb",
-  grey300: "#d1d6db",
-  grey400: "#b0b8c1",
-  grey500: "#8b95a1",
-  grey600: "#6b7684",
-  grey700: "#4e5968",
-  grey800: "#333d4b",
-  grey900: "#191f28",
-  greyOpacity50: rgba(0, 23, 51, 0.02),
-  greyOpacity100: rgba(2, 32, 71, 0.05),
-  greyOpacity200: rgba(0, 27, 55, 0.1),
-  greyOpacity300: rgba(0, 29, 58, 0.18),
-  greyOpacity400: rgba(0, 29, 54, 0.31),
-  greyOpacity500: rgba(3, 24, 50, 0.46),
-  greyOpacity600: rgba(0, 19, 43, 0.58),
-  greyOpacity700: rgba(3, 18, 40, 0.7),
-  greyOpacity800: rgba(0, 12, 30, 0.8),
-  greyOpacity900: rgba(2, 9, 19, 0.91),
-  white: "#fff",
-  black: "#000",
-  blue50: "#e8f3ff",
-  blue200: "#90c2ff",
-  blue100: "#c9e2ff",
-  blue300: "#64a8ff",
-  blue400: "#4593fc",
-  blue500: "#3182f6",
-  blue600: "#2272eb",
-  blue700: "#1b64da",
-  blue800: "#1957c2",
-  blue900: "#194aa6",
-  red50: "#fee",
-  red100: "#ffd4d6",
-  red200: "#feafb4",
-  red300: "#fb8890",
-  red400: "#f66570",
-  red500: "#f04452",
-  red600: "#e42939",
-  red700: "#d22030",
-  red800: "#bc1b2a",
-  red900: "#a51926",
-  orange50: "#fff3e0",
-  orange100: "#ffe0b2",
-  orange200: "#ffcc80",
-  orange300: "#ffbd51",
-  orange400: "#ffa927",
-  orange500: "#fe9800",
-  orange600: "#fb8800",
-  orange700: "#f57800",
-  orange800: "#ed6700",
-  orange900: "#e45600",
-  yellow50: "#fff9e7",
-  yellow100: "#ffefbf",
-  yellow200: "#ffe69b",
-  yellow300: "#ffdd78",
-  yellow400: "#ffd158",
-  yellow500: "#ffc342",
-  yellow600: "#ffb331",
-  yellow700: "#faa131",
-  yellow800: "#ee8f11",
-  yellow900: "#dd7d02",
-  purple50: "#f9f0fc",
-  purple100: "#edccf8",
-  purple200: "#da9bef",
-  purple300: "#c770e4",
-  purple400: "#b44bd7",
-  purple500: "#a234c7",
-  purple600: "#9128b4",
-  purple700: "#8222a2",
-  purple800: "#73228e",
-  purple900: "#65237b",
-  teal50: "#edf8f8",
-  teal100: "#bce9e9",
-  teal200: "#89d8d8",
-  teal300: "#58c7c7",
-  teal400: "#30b6b6",
-  teal500: "#18a5a5",
-  teal600: "#109595",
-  teal700: "#0c8585",
-  teal800: "#097575",
-  teal900: "#076565",
-  green50: "#f0faf6",
-  green100: "#aeefd5",
-  green200: "#76e4b8",
-  green300: "#3fd599",
-  green400: "#15c47e",
-  green500: "#03b26c",
-  green600: "#02a262",
-  green700: "#029359",
-  green800: "#028450",
-  green900: "#027648",
-  whiteOpacity50: rgba(209, 209, 253, 0.05),
-  whiteOpacity100: rgba(217, 217, 255, 0.11),
-  whiteOpacity200: rgba(222, 222, 255, 0.19),
-  whiteOpacity300: rgba(224, 224, 255, 0.27),
-  whiteOpacity400: rgba(232, 232, 253, 0.36),
-  whiteOpacity500: rgba(242, 242, 255, 0.47),
-  whiteOpacity600: rgba(248, 248, 255, 0.6),
-  whiteOpacity700: rgba(253, 253, 255, 0.75),
-  whiteOpacity800: rgba(253, 253, 254, 0.89),
-  whiteOpacity900: "#fff",
-  inverseGrey50: "#202027",
-  inverseGrey100: "#2c2c35",
-  inverseGrey200: "#3c3c47",
-  inverseGrey300: "#4d4d59",
-  inverseGrey400: "#62626d",
-  inverseGrey500: "#7e7e87",
-  inverseGrey600: "#9e9ea4",
-  inverseGrey700: "#c3c3c6",
-  inverseGrey800: "#e4e4e5",
-  inverseGrey900: "#fff",
-  background: "#fff",
-  darkBackground: "#17171c",
-  greyBackground: "#f2f4f6",
-  darkGreyBackground: "#101013",
-  layeredBackground: "#fff",
-  darkLayeredBackground: "#202027",
-  floatBackground: "#fff",
-  darkFloatBackground: "#2c2c35",
-};
+export const grey50 = "#f9fafb";
+export const grey100 = "#f2f4f6";
+export const grey200 = "#e5e8eb";
+export const grey300 = "#d1d6db";
+export const grey400 = "#b0b8c1";
+export const grey500 = "#8b95a1";
+export const grey600 = "#6b7684";
+export const grey700 = "#4e5968";
+export const grey800 = "#333d4b";
+export const grey900 = "#191f28";
+export const greyOpacity50 = rgba(0, 23, 51, 0.02);
+export const greyOpacity100 = rgba(2, 32, 71, 0.05);
+export const greyOpacity200 = rgba(0, 27, 55, 0.1);
+export const greyOpacity300 = rgba(0, 29, 58, 0.18);
+export const greyOpacity400 = rgba(0, 29, 54, 0.31);
+export const greyOpacity500 = rgba(3, 24, 50, 0.46);
+export const greyOpacity600 = rgba(0, 19, 43, 0.58);
+export const greyOpacity700 = rgba(3, 18, 40, 0.7);
+export const greyOpacity800 = rgba(0, 12, 30, 0.8);
+export const greyOpacity900 = rgba(2, 9, 19, 0.91);
+export const white = "#fff";
+export const black = "#000";
+export const blue50 = "#e8f3ff";
+export const blue200 = "#90c2ff";
+export const blue100 = "#c9e2ff";
+export const blue300 = "#64a8ff";
+export const blue400 = "#4593fc";
+export const blue500 = "#3182f6";
+export const blue600 = "#2272eb";
+export const blue700 = "#1b64da";
+export const blue800 = "#1957c2";
+export const blue900 = "#194aa6";
+export const red50 = "#fee";
+export const red100 = "#ffd4d6";
+export const red200 = "#feafb4";
+export const red300 = "#fb8890";
+export const red400 = "#f66570";
+export const red500 = "#f04452";
+export const red600 = "#e42939";
+export const red700 = "#d22030";
+export const red800 = "#bc1b2a";
+export const red900 = "#a51926";
+export const orange50 = "#fff3e0";
+export const orange100 = "#ffe0b2";
+export const orange200 = "#ffcc80";
+export const orange300 = "#ffbd51";
+export const orange400 = "#ffa927";
+export const orange500 = "#fe9800";
+export const orange600 = "#fb8800";
+export const orange700 = "#f57800";
+export const orange800 = "#ed6700";
+export const orange900 = "#e45600";
+export const yellow50 = "#fff9e7";
+export const yellow100 = "#ffefbf";
+export const yellow200 = "#ffe69b";
+export const yellow300 = "#ffdd78";
+export const yellow400 = "#ffd158";
+export const yellow500 = "#ffc342";
+export const yellow600 = "#ffb331";
+export const yellow700 = "#faa131";
+export const yellow800 = "#ee8f11";
+export const yellow900 = "#dd7d02";
+export const purple50 = "#f9f0fc";
+export const purple100 = "#edccf8";
+export const purple200 = "#da9bef";
+export const purple300 = "#c770e4";
+export const purple400 = "#b44bd7";
+export const purple500 = "#a234c7";
+export const purple600 = "#9128b4";
+export const purple700 = "#8222a2";
+export const purple800 = "#73228e";
+export const purple900 = "#65237b";
+export const teal50 = "#edf8f8";
+export const teal100 = "#bce9e9";
+export const teal200 = "#89d8d8";
+export const teal300 = "#58c7c7";
+export const teal400 = "#30b6b6";
+export const teal500 = "#18a5a5";
+export const teal600 = "#109595";
+export const teal700 = "#0c8585";
+export const teal800 = "#097575";
+export const teal900 = "#076565";
+export const green50 = "#f0faf6";
+export const green100 = "#aeefd5";
+export const green200 = "#76e4b8";
+export const green300 = "#3fd599";
+export const green400 = "#15c47e";
+export const green500 = "#03b26c";
+export const green600 = "#02a262";
+export const green700 = "#029359";
+export const green800 = "#028450";
+export const green900 = "#027648";
+export const whiteOpacity50 = rgba(209, 209, 253, 0.05);
+export const whiteOpacity100 = rgba(217, 217, 255, 0.11);
+export const whiteOpacity200 = rgba(222, 222, 255, 0.19);
+export const whiteOpacity300 = rgba(224, 224, 255, 0.27);
+export const whiteOpacity400 = rgba(232, 232, 253, 0.36);
+export const whiteOpacity500 = rgba(242, 242, 255, 0.47);
+export const whiteOpacity600 = rgba(248, 248, 255, 0.6);
+export const whiteOpacity700 = rgba(253, 253, 255, 0.75);
+export const whiteOpacity800 = rgba(253, 253, 254, 0.89);
+export const whiteOpacity900 = "#fff";
+export const inverseGrey50 = "#202027";
+export const inverseGrey100 = "#2c2c35";
+export const inverseGrey200 = "#3c3c47";
+export const inverseGrey300 = "#4d4d59";
+export const inverseGrey400 = "#62626d";
+export const inverseGrey500 = "#7e7e87";
+export const inverseGrey600 = "#9e9ea4";
+export const inverseGrey700 = "#c3c3c6";
+export const inverseGrey800 = "#e4e4e5";
+export const inverseGrey900 = "#fff";
+export const background = "#fff";
+export const darkBackground = "#17171c";
+export const greyBackground = "#f2f4f6";
+export const darkGreyBackground = "#101013";
+export const layeredBackground = "#fff";
+export const darkLayeredBackground = "#202027";
+export const floatBackground = "#fff";
+export const darkFloatBackground = "#2c2c35";
