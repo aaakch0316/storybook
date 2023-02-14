6e186305a5aa7474f7942415d3d8b19711bbcc37
aaakch0316 <aaakch0316@gmail.com>
2023-02-14 12:04:45 +0700

    Button [+] add dropdown button

--- src/Button.stories.tsx
+++ src/Button.stories.tsx
@@ -7,12 +7,32 @@ export default {
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => {
+  return(
+    <Button {...args}>click here</Button>
+  )
+};
+// variant='outline'
+// variant='ghost'
 
-export const Base = Template.bind({});
-Base.args = {
+export const Solid = Template.bind({});
+export const Outline = Template.bind({});
+export const Dropdown = Template.bind({});
+Solid.args = {
+  variant:'solid',
   label: 'button',
-  size: 'md',
+  size: 'sm',
   color: 'blue2'
 };
-
+Outline.args = {
+  variant:'outline',
+  label: 'button',
+  size: 'sm',
+  color: 'blue2'
+};
+Dropdown.args = {
+  variant:'dropdown',
+  label: 'button',
+  size: 'sm',
+  onClick: function() {alert('action!')}
+}
