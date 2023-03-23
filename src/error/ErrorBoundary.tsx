3519f107c0e9a6c46db38c644690c9497a30ff5f
aaakch0316@gmail.com <aaakch0316@gmail.com>
2023-03-23 23:44:45 +0900

    Error boundaty type 추가

--- src/error/ErrorBoundary.tsx
+++ src/error/ErrorBoundary.tsx
@@ -2,7 +2,32 @@
 import { css } from "@emotion/react";
 import * as React from "react";
 
-class ErrorBoundary extends React.Component {
+type ErrorBoundaryState = { error: Error | null };
+
+interface ErrorBoundaryPropsWithFallback {
+  onResetKeysChange?: (
+    prevResetKeys: Array<unknown> | undefined,
+    resetKeys: Array<unknown> | undefined
+  ) => void;
+  onReset?: (...args: Array<unknown>) => void;
+  onError?: (error: Error, info: { componentStack: string }) => void;
+  resetKeys?: Array<unknown>;
+  fallback: React.ReactElement<
+    unknown,
+    string | React.FunctionComponent | typeof React.Component
+  > | null;
+  FallbackComponent?: never;
+  fallbackRender?: never;
+}
+
+const initialState: ErrorBoundaryState = { error: null };
+
+type ErrorBoundaryProps = ErrorBoundaryPropsWithFallback;
+
+class ErrorBoundary extends React.Component<
+  React.PropsWithRef<React.PropsWithChildren<ErrorBoundaryProps>>,
+  ErrorBoundaryState
+> {
   props: any;
   state: any;
   constructor(props: any) {
