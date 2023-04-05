c02e4d6f439f916736dd0e2f33810c6525b96007
aaakch0316 <aaakch0316@gmail.com>
2023-04-05 23:33:15 +0900

    init input component 추가 계획

--- src/Input.stories.tsx
+++ src/Input.stories.tsx
@@ -1,9 +1,14 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Input from './Input';
+import Input from "./Input";
 
+/**
+ * 인풋에 버튼을 추가시킬 예정
+ * - variant는 outline으로 제작
+ * - InputRightElement 로 오른쪽에 button 추가 할 수 있도록
+ */
 export default {
-  title: 'Components/Input',
+  title: "Components/Input",
   component: Input,
 } as ComponentMeta<typeof Input>;
 
@@ -11,8 +16,7 @@ const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
 export const Base = Template.bind({});
 Base.args = {
-  label: 'Basic usag',
-  size: 'md',
-  variant: 'outline'
+  label: "Basic usag",
+  size: "md",
+  variant: "outline",
 };
-
