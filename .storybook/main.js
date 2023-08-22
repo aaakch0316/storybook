1fd1d773af30d8a0fa0ea2dc7ea4afb6aabbd775
aaakch0316 <aaakch0316@gmail.com>
2023-08-22 21:58:30 +0900

    change tsconfig 변경

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: [
     // "../src/**/*.stories.mdx",
     // "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/*.mdx",
+
     "../src/*.stories.@(js|jsx|ts|tsx)",
     "../src/*/*.stories.@(js|jsx|ts|tsx)",
     "../src/*/*/*.stories.@(js|jsx|ts|tsx)",
--- src/Textarea.tsx
+++ src/Textarea.tsx
@@ -1,34 +1,27 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
-import { Colors } from './styles/components/commons/colors'
-
+import { css } from "@emotion/react";
+import { Colors } from "./styles/components/commons/colors";
 
 interface TextareaProps {
   /**
    * Textarea placeholder
    */
   label: string;
-     /**
-    * Input border color
-    */
-      borderColor: any;
+  /**
+   * Input border color
+   */
+  borderColor: any;
 }
 
 const Textarea = (props: TextareaProps) => {
-  return (
-    <textarea css={style} placeholder={props.label} />
-  )
-}
+  return <textarea css={style} placeholder={props.label} />;
+};
 
-const style = ({
-  borderColor = 'gray7'
-}) => {
-  const border = `solid 1px ${Colors[borderColor]}`
-  return (
-    css`
-      border: ${border};
-    `
-  )
-}
+const style = ({ borderColor = "gray7" }) => {
+  const border = `solid 1px ${Colors[borderColor]}`;
+  return css`
+    border: ${border};
+  `;
+};
 
 export default Textarea;
